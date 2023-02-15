01c3e383b9a789b3f381fa91aed0052969999d4c
Haseeb <79682330+Natureboy100@users.noreply.github.com>
2023-02-15 20:13:52 +0500

    Update files 2.py

--- Python 2/files 2.py
+++ Python 2/files 2.py
@@ -1,1 +1,9 @@
+# This program is about cats
+cat_name = "Garfield"
+cat_age = 3
+cat_color = "orange"
+
+print("My cat's name is", cat_name)
+print("My cat is", cat_age, "years old")
+print("My cat's color is", cat_color)
 
